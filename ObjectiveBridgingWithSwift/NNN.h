737b99f121143f127b38d529623adc4c058b3896
= <abdullahijimoh3.ja@gmail.com>
2024-04-15 08:27:05 +0100

    Updated

---
+++ ObjectiveBridgingWithSwift/NNN.h
@@ -0,0 +1,16 @@
+//
+//  NNN.h
+//  ObjectiveBridgingWithSwift
+//
+//  Created by Abdullah on 20/01/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NNN : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ObjectiveBridgingWithSwift/NNN.m
@@ -0,0 +1,14 @@
+//
+//  NNN.m
+//  ObjectiveBridgingWithSwift
+//
+//  Created by Abdullah on 20/01/2024.
+//
+
+#import "NNN.h"
+
+@implementation NNN
+-(void)asdf{
+    
+}
+@end
